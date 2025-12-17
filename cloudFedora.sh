ed285f4fcf36c71207eff63b5256a87df4ce1d89
Yash <satorufxd@gmail.com>
2025-12-18 00:57:07 +0530

    Fix cloudFedora.sh script for installing Cloudflared

--- cloudFedora.sh
+++ cloudFedora.sh
@@ -4,13 +4,10 @@
 # Run with sudo: sudo bash install_cloudflared.sh
 
 set -e  # Exit on any error
-curl -fsSL \
-https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 \
--o cloudflared
-
+sudo dnf install -y curl
+curl -L https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 -o cloudflared
 chmod +x cloudflared
-sudo mv cloudflared /usr/local/bin/cloudflared
-
-
-
+sudo mv cloudflared /usr/local/bin/
 cloudflared --version
+
+
